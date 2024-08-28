3867b3b637470d1628f5137d83c67d1aa6c3d165
Everton Carvalho <evertooon03@gmail.com>
2024-08-27 23:06:14 -0300

    Conclusao principais rotas

---
+++ Produto/Entidade/Produto.cs
@@ -0,0 +1,15 @@
+﻿namespace Produto_API.Entidade
+{
+    public class Produto
+
+    {
+        public string Id { get; set; }
+        public string? Nome { get; set; }
+        public decimal? Preco { get; set; }
+        public string? Categoria { get; set; }
+        public bool Disponivel { get; set; }
+        public DateTime? Validade { get; set; }
+        
+
+    }
+}
